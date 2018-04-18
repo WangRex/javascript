b0098ce40ae7c0bd2cb2c511922d21dc369f6d68
wangrex <503431920@qq.com>
2018-04-18 15:39:43 +0800

    Add GetQuery

--- regular/index.js
+++ regular/index.js
@@ -9,3 +9,11 @@ console.log(regexp.test(str));
 var myRe = /d(b+)d/g;
 var myArray = myRe.exec("aacdbbdbsbz");
 console.log(myArray.index);
+
+
+String.prototype.getQuery = function (name) {
+    var reg = new RegExp('(^|\\?|&)' + name + '=([^&]*|$)'), url = this.replace(/&/g, '&'), r = url.match(reg);
+    return r === null ? null : unescape(r[2]);
+}
+var str = 'http://wpa.qq.com/msgrd?v=3&uin=1438344583&site=qq&menu=yes';
+alert(str.getQuery('uin')); 
